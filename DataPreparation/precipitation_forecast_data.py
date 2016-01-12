72902bafc326e1069ab58ff89a1972ac4fbdc603
Mohamed M. Morsy <Mohamed M. Morsy>
2016-01-11 21:38:58 -0500

    Extracted the required data for a point and created CSV file to include the time series data

--- DataPreparation/precipitation_forecast_data.py
+++ DataPreparation/precipitation_forecast_data.py
@@ -1,5 +1,9 @@
 __author__ = 'Mohamed Morsy'
 import urllib
+from xml.etree import ElementTree
+from datetime import datetime
+import csv
+
 
 
 # point of interest
@@ -14,6 +18,31 @@ precipURL = "http://graphical.weather.gov/xml/sample_products/browser_interface/
             + "&product=time-series&begin=2004-01-01T00%3A00%3A00&end=2020-01-04T00%3A00%3A00&Unit=e&qpf=qpf&Submit=Submit"
 
 precipData = urllib.urlopen(precipURL)
-readPrecipData = precipData.read()
 
-# TO DO: extract the desired forecast data from the respond as times series (time (HH:MM) and Value )
+#Read the response
+readPrecipData = precipData.read()
+tree = ElementTree.fromstring(readPrecipData)
+
+# Extract the precipitation data
+values = []
+for value in tree.getiterator('value'):
+    values.append(value.text)
+
+#Extract the precipitation data corresponding date, time and time step
+date = []
+time = []
+dtime = []
+timestep = []
+n = 0
+for dt in tree.getiterator('start-valid-time'):
+    date.append(datetime.date(datetime.strptime(dt.text[:-6], '%Y-%m-%dT%H:%M:%S')))
+    time.append(datetime.time(datetime.strptime(dt.text[:-6], '%Y-%m-%dT%H:%M:%S')))
+    dtime.append(datetime.strptime(dt.text[:-6], '%Y-%m-%dT%H:%M:%S'))
+    timestep.append(dtime[n]-dtime[n-1])
+    n += 1
+
+#Create CSV file to append the data and create the time series
+c = csv.writer(open("precip.csv", "wb"))
+c.writerow(["Date","Time","Time Step","Value"])
+for i in range(len(values)):
+    c.writerow([str(date[i]),str(time[i]),str(timestep[i]),values[i]])
