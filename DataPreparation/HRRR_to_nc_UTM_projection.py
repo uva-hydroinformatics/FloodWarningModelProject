51df5fdd316adbdb4bdb6afc4c38f6afa1c415fc
Jeff Sadler <Jeff Sadler>
2016-11-02 12:51:41 -0400

    makes netcdf files from rasters

--- DataPreparation/HRRR_to_nc_UTM_projection.py
+++ DataPreparation/HRRR_to_nc_UTM_projection.py
@@ -10,14 +10,11 @@ Authors: Gina O'Neil, Mohamed Morsy, Jeff Sadler
 """
 import numpy as np
 from pydap.client import open_url
-import matplotlib.dates
-import xray
 import datetime as dt
-import sys
-import utm
 from osgeo import gdal, osr, ogr
 import numpy as np
 import os
+from netCDF4 import Dataset
 
 """
 Global parameters:
@@ -48,24 +45,73 @@ def getData(current_dt):
             date = dt.datetime.strftime(dtime_fix,"%Y%m%d")
             fc_hour = dt.datetime.strftime(dtime_fix, "%H")
             hour = str(fc_hour)
-            url = 'http://nomads.ncep.noaa.gov:9090/dods/hrrr/hrrr%s/hrrr_sfc_%sz'%(date,hour)
+            url = 'http://nomads.ncep.noaa.gov:9090/dods/hrrr/hrrr%s/hrrr_sfc_%sz' % (date, hour)
             dataset = open_url(url)
             if len(dataset.keys()) > 0:
                 False
                 return(dataset, url, date, hour)
             else:
-                print "Back up method - Failed to open : %s"%(url)
+                print "Back up method - Failed to open : %s" % url
                 delta_T += 1
                 True
         except:
             delta_T += 1
-            print "Failed to open : %s"%(url)
+            print "Failed to open : %s" % url
+
 
 def gridpt(myVal, initVal, aResVal):
     gridVal = int((myVal-initVal)/aResVal)
     return gridVal
 
-    
+
+def make_wgs_raster(grid, hr):
+    xres = grid.lon[1] - grid.lon[0]
+    yres = grid.lat[1] - grid.lat[0]
+    ysize = len(grid.lat)
+    xsize = len(grid.lon)
+    ulx = grid.lon[0] - (xres / 2.)
+    uly = grid.lat[0] - (yres / 2.)
+    driver = gdal.GetDriverByName('GTiff')
+    srs = osr.SpatialReference()
+    srs.ImportFromEPSG(4326)
+    projected_srs = osr.SpatialReference()
+    projected_srs.ImportFromEPSG(4269)
+    projected_srs.SetUTM(18, True)
+    gt = [ulx, xres, 0, uly, 0, yres]
+    precip_array = grid.apcpsfc.data[0]
+    precip_array = precip_array.astype(np.float64)
+    latlonfile = 'latlon%s.tif' % hr
+    ds = driver.Create(latlonfile, xsize, ysize, 1, gdal.GDT_Float64, )
+    ds.SetProjection(srs.ExportToWkt())
+    ds.SetGeoTransform(gt)
+    outband = ds.GetRasterBand(1)
+    outband.SetStatistics(np.min(precip_array),
+                          np.max(precip_array),
+                          np.average(precip_array),
+                          np.std(precip_array))
+    outband.WriteArray(precip_array)
+    ds = None
+    return latlonfile
+
+
+def project_to_utm(wgs_raster_name, hr):
+    outfilename = "projected%s.tif" % hr
+    os.system('gdalwarp %s %s -t_srs "+proj=utm +zone=18 +datum=NAD83"' % (wgs_raster_name, outfilename))
+    return outfilename
+
+
+def get_projected_array(grid, hr):
+    wgs_file = make_wgs_raster(grid, hr)
+    projected_file_name = project_to_utm(wgs_file, hr)
+    ds = gdal.Open(projected_file_name)
+    precip = ds.ReadAsArray()
+    ny, nx = np.shape(precip)
+    b = ds.GetGeoTransform()  # bbox, interval
+    x = np.arange(nx) * b[1] + b[0]
+    y = np.arange(ny) * b[5] + b[3]
+    return x, y, precip
+
+
 def main():
     
     # Get newest available HRRR dataset by trying (current datetime - delta time) until
@@ -73,9 +119,9 @@ def main():
     # of HRRR datasets to repository
     dtime_now = dt.datetime.utcnow()
     print "Open a connection to HRRR to retrieve forecast rainfall data.............\n"
-    #get newest available dataset
+    # get newest available dataset
     dataset, url, date, hour = getData(dtime_now)
-    print ("Retrieving forecast data from: %s " %(url))
+    print ("Retrieving forecast data from: %s " % url)
 
     # select desired forecast product from grid, grid dimensions are time, lat, lon
     # apcpsfc = "surface total precipitation" [mm]
@@ -92,44 +138,33 @@ def main():
     
     # Convert time steps from decimal days to datetime format
     #  must keep array items as datetime objects
-    #times = [ matplotlib.dates.num2date(t-1) for t in precip.time[:] ]
+    # times = [ matplotlib.dates.num2date(t-1) for t in precip.time[:] ]
     # hours = []
     # for t in times:
     #   hours.append(int(dt.datetime.strftime(times[t], "%H")))
     grid = precip[0, grid_lat1:grid_lat2, grid_lon1:grid_lon2]
-    xres = grid.lon[1] - grid.lon[0]
-    yres = grid.lat[1] - grid.lat[0]
-    ysize = len(grid.lat)
-    xsize = len(grid.lon)
-    ulx = grid.lon[0] - (xres / 2.)
-    uly = grid.lat[0] - (yres / 2.)
-    driver = gdal.GetDriverByName('GTiff')
-    srs = osr.SpatialReference()
-    srs.ImportFromEPSG(4326)
-    projected_srs = osr.SpatialReference()
-    projected_srs.ImportFromEPSG(4269)
-    projected_srs.SetUTM(18, True)
-    gt = [ulx, xres, 0, uly, 0, yres]
+    x, y, precip_prj = get_projected_array(grid, 0)
+    nco = Dataset('%s.nc' % dt.datetime.now().strftime('%Y%m%d.%H%M'), mode='w')
+    nco.createDimension('X', len(x))
+    nco.createDimension('Y', len(y))
+    nco.createDimension('time', None)
+
+    x_var = nco.createVariable('X', 'f4', ('X',))
+    y_var = nco.createVariable('Y', 'f4', ('Y',))
+    rain = nco.createVariable('Rainfall', 'f4', ('time', 'X', 'Y'))
+
+    x_var[:] = x
+    y_var[:] = y
 
     for hr in range(len(precip.time[:])):
         grid = precip[hr, grid_lat1:grid_lat2, grid_lon1:grid_lon2]
-        precip_array = grid.apcpsfc.data[0]
-        precip_array = precip_array.astype(np.float64)
-        latlonfile = 'latlon{}.tif'.format(hr)
-        ds = driver.Create(latlonfile, xsize, ysize, 1, gdal.GDT_Float64, )
-        ds.SetProjection(srs.ExportToWkt())
-        ds.SetGeoTransform(gt)
-        outband=ds.GetRasterBand(1)
-        outband.SetStatistics(np.min(precip_array),
-                              np.max(precip_array),
-                              np.average(precip_array),
-                              np.std(precip_array))
-        outband.WriteArray(precip_array)
-        ds = None
-        outfilename = "projected{}.tif".format(hr)
-        os.system('gdalwarp {} {} -t_srs "+proj=utm +zone=18 +datum=NAD83"'.format(latlonfile, outfilename))
-        print ("File for hour %d has been written" %(hr))
+        x, y, precip_prj = get_projected_array(grid, hr)
+        precip_prj = np.transpose(precip_prj)
+        rain[hr, :, :] = precip_prj
 
+        print ("File for hour %d has been written" % hr)
+
+    nco.close()
 
 if __name__=="__main__":
    main()
