4b5089b679c335c74ee3dbe7461133c0e73e32f4
Danny Voce <davjr96@gmail.com>
2018-01-06 15:04:38 -0500

    new branch

--- PhaseI/DataPreparation/run_BC_data_prep.bat
+++
@@ -1,8 +0,0 @@
-@ECHO off
-SETLOCAL
-
-REM change path to BC_data_prep
-
-python.exe C:\Users\CIVIL\Desktop\HEC-HMS_EXAMPLE\HEC-HMS_EXAMPLE\BC_data_prep.py
-
-ENDLOCAL
--- PhaseI/DataPreparation/run_WriteFromCSV_loop.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-SETLOCAL
-
-REM change filepath to Jython exe and to DSSVue script
-
-start C:\"Program Files (x86)\HEC\HEC-DSSVue-v2.6.00.59\HEC-DSSVue-v2.6.00.59\Jython.exe"  C:\Users\CIVIL\AppData\Roaming\HEC\HEC-DSSVue\scripts\WriteFromCSV_loop.py %1 exit
-
-ENDLOCAL
